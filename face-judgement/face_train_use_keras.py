a4dae8b960fe3bb511183885c610557ae6acacaf
zhengtianqi <zhengtianqi@gooagoo.com>
2019-11-23 13:31:32 +0800

    face-predict-learn-complete

--- face-judgement/face_train_use_keras.py
+++ face-judgement/face_train_use_keras.py
@@ -125,14 +125,93 @@ class Model:
         self.model.add(Dropout(0.5))  # 16 Dropout层
         self.model.add(Dense(nb_classes))  # 17 Dense层
         self.model.add(Activation('softmax'))  # 18 分类层，输出最终结果
-
         # 输出模型概况
         self.model.summary()
+
+    # 训练模型
+    def train(self, dataset, batch_size=20, nb_epoch=10, data_augmentation=True):
+        sgd = SGD(lr=0.01, decay=1e-6,
+                  momentum=0.9, nesterov=True)  # 采用SGD+momentum的优化器进行训练，首先生成一个优化器对象
+        self.model.compile(loss='categorical_crossentropy',
+                           optimizer=sgd,
+                           metrics=['accuracy'])  # 完成实际的模型配置工作
+
+        # 不使用数据提升，所谓的提升就是从我们提供的训练数据中利用旋转、翻转、加噪声等方法创造新的
+        # 训练数据，有意识的提升训练数据规模，增加模型训练量
+        if not data_augmentation:
+            self.model.fit(dataset.train_images,
+                           dataset.train_labels,
+                           batch_size=batch_size,
+                           nb_epoch=nb_epoch,
+                           validation_data=(dataset.valid_images, dataset.valid_labels),
+                           shuffle=True)
+        # 使用实时数据提升
+        else:
+            # 定义数据生成器用于数据提升，其返回一个生成器对象datagen，datagen每被调用一
+            # 次其生成一组数据（顺序生成），节省内存，其实就是python的数据生成器
+            datagen = ImageDataGenerator(
+                featurewise_center=False,  # 是否使输入数据去中心化（均值为0），
+                samplewise_center=False,  # 是否使输入数据的每个样本均值为0
+                featurewise_std_normalization=False,  # 是否数据标准化（输入数据除以数据集的标准差）
+                samplewise_std_normalization=False,  # 是否将每个样本数据除以自身的标准差
+                zca_whitening=False,  # 是否对输入数据施以ZCA白化
+                rotation_range=20,  # 数据提升时图片随机转动的角度(范围为0～180)
+                width_shift_range=0.2,  # 数据提升时图片水平偏移的幅度（单位为图片宽度的占比，0~1之间的浮点数）
+                height_shift_range=0.2,  # 同上，只不过这里是垂直
+                horizontal_flip=True,  # 是否进行随机水平翻转
+                vertical_flip=False)  # 是否进行随机垂直翻转
+
+            # 计算整个训练样本集的数量以用于特征值归一化、ZCA白化等处理
+            datagen.fit(dataset.train_images)
+
+            # 利用生成器开始训练模型
+            self.model.fit_generator(datagen.flow(dataset.train_images, dataset.train_labels,
+                                                  batch_size=batch_size),
+                                     samples_per_epoch=dataset.train_images.shape[0],
+                                     nb_epoch=nb_epoch,
+                                     validation_data=(dataset.valid_images, dataset.valid_labels))
+            # 识别人脸
+
+    def face_predict(self, image):
+        # 依然是根据后端系统确定维度顺序
+        if K.image_dim_ordering() == 'th' and image.shape != (1, 3, IMAGE_SIZE, IMAGE_SIZE):
+            image = resize_image(image)  # 尺寸必须与训练集一致都应该是IMAGE_SIZE x IMAGE_SIZE
+            image = image.reshape((1, 3, IMAGE_SIZE, IMAGE_SIZE))  # 与模型训练不同，这次只是针对1张图片进行预测
+        elif K.image_dim_ordering() == 'tf' and image.shape != (1, IMAGE_SIZE, IMAGE_SIZE, 3):
+            image = resize_image(image)
+            image = image.reshape((1, IMAGE_SIZE, IMAGE_SIZE, 3))
+        # 浮点并归一化
+        image = image.astype('float32')
+        image /= 255
+        # 给出输入属于各个类别的概率，我们是二值类别，则该函数会给出输入图像属于0和1的概率各为多少
+        result = self.model.predict_proba(image)
+        print('result:', result)
+        # 给出类别预测：0或者1
+        result = self.model.predict_classes(image)
+        # 返回类别预测结果
+        return result[0]
+
+    MODEL_PATH = 'D:\Idea-workspace\model\me.face.model.h5'
+
+    def save_model(self, file_path=MODEL_PATH):
+        self.model.save(file_path)
+
+    def load_model(self, file_path=MODEL_PATH):
+        self.model = load_model(file_path)
+
+    def evaluate(self, dataset):
+        score = self.model.evaluate(dataset.test_images, dataset.test_labels, verbose=1)
+        print("%s: %.2f%%" % (self.model.metrics_names[1], score[1] * 100))
 
 
 if __name__ == '__main__':
     dataset = Dataset('D:\Idea-workspace\photo')
     dataset.load()
-
     model = Model()
-    model.build_model(dataset)
+    # 训练模型
+    # model.build_model(dataset)  # 先前添加的测试build_model()函数的代码
+    # model.train(dataset)  # 测试训练函数的代码
+    # model.save_model()  # 保存训练的模型
+    # 评估模型
+    model.load_model()
+    model.evaluate(dataset)
